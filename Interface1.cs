0ecc028f06f2e1c7d6f504e825f820ce6156a657
Hurttaj <hurttaj@gmail.com>
2016-03-22 00:49:57 +0200

    Added files via upload

---
+++ Interface1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class CollisionObject
+    {
+        double speed;
+        double radius;
+        int xPosition;
+        int yPosition;
+
+        public double Speed
+        {   get
+            { return speed; }
+            set
+            {
+                if (value >= 0) speed = value;
+                else throw new System.ArgumentException("Cannot be negatve."); }
+        }
+
+        public double Radius
+        {
+            get
+            { return speed; }
+            set
+            {
+                if (value >= 0) radius = value;
+                else throw new System.ArgumentException("Cannot be negatve.");
+            }
+        }
+
+        public void Collision(CollisionObject, CollisionObject)
+        {
+            Console.WriteLine("Do something!");
+        }
+
+        public void move()
+        {
+            Console.WriteLine("Do something!");
+        }
+
+    }
+}
