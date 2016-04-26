c504e547569ed04b86057510181177d6015d5e7f
Juuso Minkkilä <K1756@student.jamk.fi>
2016-04-26 12:58:59 +0300

    Comments.

--- CrimsonClone/CrimsonClone/Classes/Enemy.cs
+++ CrimsonClone/CrimsonClone/Classes/Enemy.cs
@@ -7,47 +7,64 @@ using System.Threading.Tasks;
 
 namespace CrimsonClone.Classes
 {
+    /// <summary>
+    /// This is a class for enemy characters
+    /// It inherits collision object
+    /// </summary>
     public class Enemy : CollisionObject
     {
-        // maxSpeed is overall maximum speed
+        /// <summary>
+        /// In order of appearance:
+        /// maxSpeed is overall maximum speed
+        /// currentMaxSpeed is the maximum speed for each enemy
+        /// accelerate is enemy's acceleration
+        /// speed is enemy's current speed
+        /// </summary>
         private readonly double maxSpeed = 50.0;
-        // currentMaxSpeed is the maximum speed for each enemy
         private double currentMaxSpeed = 4.0;
         private readonly double accelerate = 0.5;
         private double speed = 0;
 
-        // constructor
+        /// <summary>
+        /// Enemy's default constructor
+        /// </summary>
+        /// <param name="radius">required for collision checks</param>
+        /// <param name="tickCount">increases maximum speed</param>
         public Enemy(double radius, int tickCount)
         {
             Radius = radius;
             currentMaxSpeed += tickCount / 1800.0;
             Debug.WriteLine("MaxSpeed" + currentMaxSpeed);
-            // safetyguard for enemy speed
+            /// safetyguard for enemy speed
             if (currentMaxSpeed > maxSpeed) currentMaxSpeed = maxSpeed;
         }
 
-        // this is a method for enemy movement
+        /// <summary>
+        /// this is a method for enemy movement
+        /// </summary>
+        /// <param name="dirX">player's X-position</param>
+        /// <param name="dirY">player's Y-position</param>
         public void Move(float dirX, float dirY)
         {
-            // speed settings
+            /// speed settings
             speed += accelerate;
             if (speed > currentMaxSpeed) speed = currentMaxSpeed;
 
-            // temporary variables for initial X and Y changes and for player location
+            /// temporary variables for initial X and Y changes and for player location
             double deltaX = 0;
             double deltaY = 0;
 
-            // calculating the difference between player and enemy coordinates
+            /// calculating the difference between player and enemy coordinates
             deltaX = dirX - PositionX;
             deltaY = dirY - PositionY;
 
-            // calculating the angle between the player and the enemy in radians
+            /// calculating the angle between the player and the enemy in radians
             double angle = Math.Atan(deltaY / deltaX);
 
             if (deltaX < 0) angle += Math.PI;
 
-            // actual X and Y coordinate calculations
-            // sign returns 1 if given number is positive, and -1 if it's negative
+            /// actual X and Y coordinate calculations
+            /// sign returns 1 if given number is positive, and -1 if it's negative
             if (deltaX == 0 && deltaY != 0)
             {
                 PositionY += (float)speed * Math.Sign(deltaY);
